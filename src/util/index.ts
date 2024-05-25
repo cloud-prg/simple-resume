30a29dcd9b92a8bf606455c4c28056296ee9fe93
cloud_prg <cloud_prg@163.com>
2024-05-25 16:07:38 +0800

    feat: 自动生成jit文件脚本&放入构建周期

---
+++ src/util/index.ts
@@ -0,0 +1,58 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const UTIL_DIR = process.cwd();
+const PROJECT_DIR = path.join(UTIL_DIR.split('src')[0]);
+const PUBLIC_DIR = path.join(PROJECT_DIR, 'public');
+
+// /**
+//  * 检测指定文件是否存在于 public 目录中，如果不存在则执行指定命令
+//  * @param {string} filePath 要检测的文件路径，相对于 public 目录
+//  * @param {string} command 要执行的命令
+//  * @returns {Promise<void>} 操作完成后的 Promise
+//  */
+export function checkPublicFileAndExec(filename:string, command:string) {
+    const fullPath = `${PUBLIC_DIR}/${filename}`;
+
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(fullPath)) {
+            // console.log(`File ${filename} already exists in public directory.`);
+            resolve(`File ${filename} already exists in public directory.`);
+        } else {
+            // console.log(`File ${filename} not found in public directory. Executing command: _`);
+            executeCommand(command);
+            reject(`File ${filename} not found in public directory. Executing command: _`);
+        }
+    });
+}
+// /**
+//  * 执行指定的命令
+//  * @param {string} command 要执行的命令
+//  * @returns {Promise<void>} 操作完成后的 Promise
+//  */
+export function executeCommand(command:string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(`Error executing command: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                reject(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            resolve();
+        });
+    });
+}
+
+export async function autoGenerateTailwindJIT() {
+    try {
+        await checkPublicFileAndExec('tailwind.css', `npx tailwindcss -o ${PUBLIC_DIR}/tailwind.css --watch`)
+        console.log('File check and execute completed.');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { autoGenerateTailwindJIT } from './src/util'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4300,
   },
-  plugins: [react()],
-  resolve:{
+  plugins: [react(),
+  {
+    name: 'generate-tailwind-css',
+    buildStart() {
+      autoGenerateTailwindJIT()
+    }
+  }
+  ],
+  resolve: {
     alias: {
       '@': '/src',
     }
