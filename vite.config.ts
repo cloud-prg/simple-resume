d1a8c72e2b418620678efdd0ae90146f2b0631a8
cloud_prg <cloud_prg@163.com>
2024-05-25 21:02:35 +0800

    chore: baseUrl config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { autoGenerateTailwindJIT } from './src/util'
 
+const isDev = process.env.NODE_ENV === 'development'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isDev ? '' : '/simple-resume/',
   server: {
     port: 4300,
   },
