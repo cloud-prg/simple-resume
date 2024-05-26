6140c4a95bbb80ed0a36c8fe67a79183e929bf5b
cloud_prg <cloud_prg@163.com>
2024-05-26 11:55:35 +0800

    chore: update

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,18 @@
 set -e
 
 # 生成静态文件
-npm run build   
+pnpm run build   
 
+pwd
 # 进入生成的文件夹
 cd ./dist
 
+pwd
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:cloud-prg/easy-record.git master:gh-pages
 
 cd -
